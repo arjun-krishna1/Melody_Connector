560354ff6ae56d135435bf74b4da12c4e6f7987e
arjunkrishna <arjun.krishna@huawei.com>
2021-07-03 14:28:32 -0400

    add midi playing

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 music_vae = new mm.MusicVAE('https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_4bar_small_q2');
 music_vae.initialize();
 
-const vaePlayer = new mm.Player();
+const player = new mm.Player();
 
-vae_temperature = 1.5;
+const url1 = "music/another_one_bites_the_dust_simple.mid";
+const url2 = "music/hey_jude.mid";
 
-function playVAE() {
-    if (vaePlayer.isPlaying()) {
-        vaePlayer.stop();
-        return;
-    }
-    music_vae
-    .sample(1, vae_temperature)
-    .then((sample) => vaePlayer.start(sample[0]));
-}
+// vae_temperature = 1.5;
+
+// function playVAE() {
+//     if (player.isPlaying()) {
+//         player.stop();
+//         return;
+//     }
+//     music_vae
+//     .sample(1, vae_temperature)
+//     .then((sample) => player.start(sample[0]));
+// }
 
 let playButton = document.getElementById("playButton");
 
-playButton.onclick = function () {
-  console.log("playing");
-  const thisSeq = DRUMS;
-  player.start(thisSeq);
-  play(thisSeq);
+let seq1; let seq2;
+
+async function loadSequences() {
+  seq1 = await mm.urlToNoteSequence(url1);
+  seq2 = await mm.urlToNoteSequence(url2);
+}
+
+loadSequences();
+
+playButton.onclick = async function () {
+  player.start(seq2);
 };
